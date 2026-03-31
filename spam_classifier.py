c4e9054ce6e8059f8bd85cfa0a89aa38aa3130ea
Samuel Kabuya <samuelkabuya408@gmail.com>
2026-03-30 21:30:18 -0400

    Task 2 done with basic exploration such as feature selection, data clean check, checking class distribution, checking dataset shape and checking missing values. I picked content and class as they are the most important out of the whole dataset. CLASS  will be our target and CONTENT will serve as the features.

--- spam_classifier.py
+++ spam_classifier.py
@@ -1,10 +1,30 @@
 import pandas as pd
 
-
 #Task 1: Loading the data and looking at it
 df = pd.read_csv('Youtube02-KatyPerry.csv')
 
 print("Looking at data")
 print(df.head())
 
+#Task 2 Basic Data Exploration
+#2.1 Feature selection
+df = df[['CONTENT', 'CLASS']]
 
+#2.2 Checking if we deleted stuff we didnt need
+print("dataset after deleting columns")
+print(df.head())
+
+#2.3 Checking class distribution
+print("Checking balance of classes")
+counts = df['CLASS'].value_counts()
+print("Normal messages: " + str(counts[0]))
+print("Spam messages: " + str(counts[1]))
+
+#2.4 Checking dataset shape
+print("Dataset shape:")
+print(str(df.shape[0]) + " rows")
+print(str(df.shape[1]) + " columns")
+
+#2.5 Checking for missing values
+print("Checking for missing values")    
+print(df.isnull().sum())
